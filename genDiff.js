57884070d347071923df9660a5c0a6570950e77c
Z <zhenisbusy@qq.com>
2023-09-06 16:03:55 +0800

    自动生成脚本

--- genDiff.js
+++ genDiff.js
@@ -1,29 +1,51 @@
-/**
- * 1. 根据
- */
-
 const { execSync } = require('child_process')
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
+const path = require('path')
 
-const data = readFileSync('./diff.json').toString()
-
-const result = {}
-const rows = execSync('git diff origin/master --name-status')
-  .toString()
-  
-if (rows.length > 0) {
-  rows
+function main () {
+  const diff = { A: {}, M: {}, D: [] }
+  execSync('git diff origin/master --name-status')
+    .toString()
     .split(/\n/)
     .filter(Boolean)
     .forEach(row => {
-    const status = row.slice(0, 1), filePath = row.slice(2)
-    // 只处理markdown文件
-    if (/\.md/.test(filePath)) {
-      // 读取文件
-    }
-    if (!result[status]) result[status] = []
-    result[status].push(filePath)
-  })
+      const status = row.slice(0, 1), filePath = row.slice(2)
+      const filename = path.basename(filePath, '.md')
+      if (!/\.md/.test(filePath)) return
+      if (status === 'D') diff.D.push(filename)
+      else if (status === 'A' || status === 'M') {
+        const record = status === 'A' ? diff.A : diff.M
+        if (record[filename]) {
+          console.error("重复文件名！", filename)
+          return
+        }
+        const text = readFileSync(filePath).toString()
+        const data = parseArticle(text)
+        if (!data) {
+          console.error("解析失败！")
+          return
+        }
+        data.title = filename
+        record[filename] = data
+      }
+    })
+  writeFileSync('./diff.json', JSON.stringify(diff))
+  execSync('git add diff.json | git commit diff.json -m "Article数据自动生成"')
 }
 
-console.log(result)
+function parseArticle (text) {
+  const result = { title: "", content: text }
+  let info = text.match(/^```\s*zhenisbusy(.*)```$/)
+  if (info && info[1]) {
+    result.content = text.replace(info[0], '').trimStart()
+    try {
+      const infoToJson = JSON.parse(info[1])
+      Object.assign(result, infoToJson)
+    } catch {
+      return void 0
+    }
+  }
+  return result
+}
+
+main()
