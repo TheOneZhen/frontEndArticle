a7960c80e4e29886b9b1479d1c347b778d049d70
Z <zhenisbusy@qq.com>
2024-01-30 20:43:25 +0800

    test

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
-const { readdirSync, statSync, copyFileSync, existsSync, mkdirSync } = require('fs')
+const { readdirSync, statSync, copyFileSync, existsSync, mkdirSync, writeFileSync } = require('fs')
 const { resolve } = require('path')
-const { update } = require('package.json')
+
 /**
  * Github Actions只负责打包，然后将文件变更信息交给后端处理。
  */
 
 const __DIST__ = 'dist'
-const __RESOURCE__ = resolve(__DIST__, 'resource')
+const __STATIC__ = resolve(__DIST__, 'static')
 const __ARTICLE__ = resolve(__DIST__, 'article')
 
 /**
@@ -19,38 +19,38 @@ function move (targetDir) {
       const path = resolve(targetDir, name)
       const status = statSync(path)
       if (status.isDirectory()) move(path)
-      else if (/\.(png|svg|jpg|jpeg)/i.test(name)) copyFileSync(path, resolve(__RESOURCE__, name))
-      else if (/\.md/i.test(name)) copyFileSync(path, resolve(__ARTICLE__, name))
+      else if (/\.(png|svg|jpg|jpeg)/i.test(name)) copyFileSync(path, resolve(__STATIC__, name))
     })
 }
-/**
- * 生成比较文件（diff.json）。每次文章更新时，`package.json`会设置一个`update`属性用来设置更新的内容（包含标签的更新），这样可以保证内容的可控。
- * @param {string} beforeCommit 
- * @param {string} afterCommit 
- */
-function genDiff (beforeCommit = '', afterCommit = '') {
-  const diff = { A: [], C: [], M: [], D: [],}
+
+function genUpadte () {
+  /**  */
+  /** 后端处理该数据顺序：先删除，再新增，最后修改 */
+  const diff = { add/** 新增 */: {}, modify/** 修改 */: {}, delete/** 删除 */: [], else/** 其他类型 */: [] }
   execSync('git config --global core.quotepath false')
-  execSync(`git diff ${beforeCommit} ${afterCommit} --name-status`)
+  execSync(`git diff ${ process.argv[2] || '' } ${ process.argv[3] || '' } --name-status`)
     .toString()
     .split(/\n/)
     .filter(Boolean)
     .forEach(row => {
-      const sta
+      const status = row.slice(0, 1), tokens = row.split(' ').filter(Boolean)
+      /** 如果是重命名，需要先删除再添加 */
+      if (status === 'R') {
+        const oldName = 
+      }
     })
 }
 
 mkdirSync(__DIST__)
-mkdirSync(__RESOURCE__)
+mkdirSync(__STATIC__)
 mkdirSync(__ARTICLE__)
 
 move('./articles')
 
-update !== undefined && genDiff (process.argv[2], process.argv[3])
+genUpadte()
 /**
  * result directory S
  * - dist
- *  - resource
- *  - article
- *  - diff.json
+ *  - static
+ *  - update.json
  */
